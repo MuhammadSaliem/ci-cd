bf3e5b394064f7ca8b0402e055c9d2d21e9276b1
_Saleem_ <mohamedselim701@gmail.com>
2024-09-23 23:13:57 +0300

    added MathOperationsService

---
+++ src/main/java/com/example/demo/MathOperationsService.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MathOperationsService {
+
+    public Integer sumList(List<Integer> numList) {
+        Integer sum = 0; // To store the result
+
+        for (int i = 0; i < numList.size(); i++) {
+            sum += numList.get(i);
+        }
+
+        return sum;
+    }
+}
