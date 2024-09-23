41b08b3f39ee661d44c72024a222a728d6846c11
_Saleem_ <mohamedselim701@gmail.com>
2024-09-23 22:48:18 +0300

    added /sum endpoint it takes a list of numbers and return their sum

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,17 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello there, World!";
+    }
+
+    @GetMapping("/sum")
+    public Integer sumList(@RequestBody List<Integer> numList) {
+        Integer sum = 0;
+
+        for(int i = 0; i < numList.size(); i++)
+        {
+            sum += numList.get(i);
+        }
+
+        return sum;
     }
 }
