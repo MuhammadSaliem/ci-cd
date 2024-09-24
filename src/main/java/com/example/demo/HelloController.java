2c856b347b2df508305284d57de3551dc09adab1
_Saleem_ <mohamedselim701@gmail.com>
2024-09-25 00:35:57 +0300

    remove depedency mathOperationService

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -38,4 +38,37 @@ public class HelloController {
 
         return sum;
     }
+
+    @PostMapping("/subract")
+    public Double subList(@RequestBody List<Integer> numList) {
+        Double sum = 0.0;
+
+        for (int i = 0; i < numList.size(); i++) {
+            sum += numList.get(i);
+        }
+
+        return sum;
+    }
+
+    @PostMapping("/multiple")
+    public Double mulList(@RequestBody List<Integer> numList) {
+        Double sum = 0.0;
+
+        for (int i = 0; i < numList.size(); i++) {
+            sum += numList.get(i);
+        }
+
+        return sum;
+    }
+
+    @PostMapping("/pow")
+    public Double powList(@RequestBody List<Integer> numList) {
+        Double sum = 0.0;
+
+        for (int i = 0; i < numList.size(); i++) {
+            sum += numList.get(i);
+        }
+
+        return sum;
+    }
 }
